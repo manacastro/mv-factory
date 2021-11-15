98541707539594a873d34dd8a257791a68fd45dc
Mariana Castro <mb.castro17@gmail.com>
2021-11-15 16:41:38 -0300

    Se organizan componentes para la busqueda de ciudades

--- src/componentes/CitySection/CitySection.js
+++ src/componentes/CitySection/CitySection.js
@@ -1,23 +1,55 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Checkbox from '../Checkbox/Checkbox';
-import Search from '../Search/Search';
-import Dropdown from '../Dropdown/Dropdown';
+import Form from "react-bootstrap/Form";
+import './CitySection.scss'
+import Button from "react-bootstrap/Button";
 
-const CitySection = () => (
-    
-    <Row>
-        <Col col lg-6>
-            <Dropdown />         
-        </Col>
-        <Col col lg-2>            
-            <Checkbox />
-        </Col>
-        <Col col lg-4>
-            <Search />
-        </Col>
-    </Row>
-    
-)
+class CitySection extends React.Component {
+    constructor() {
+        super();
+        this.state = { cities: [] };
+        //this.handleChange = this.handleChange.bind(this);
+    }
+
+    // handleChange(event) {
+    //     this.setState({value: event.target.cities});
+    // }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/getcities", {
+            mode: 'cors'
+        })
+            .then(response => response.json())
+            .then(data => this.setState({ cities: data.cities }))
+    }
+
+    render() {
+        console.log(this.state)
+        if (this.state.cities.length > 0) {
+            return (
+               
+                <div class="citySearch">
+                    <div class="city">
+                        <Form.Select aria-label="Default select example" className="dropdown" >
+                            <option>Seleccione la ciudad</option>
+                            {this.state.cities.map(cities => (
+                                <option id={cities.id} value={cities.description}>
+                                    {cities.description}
+                                </option>
+                            ))}
+                        </Form.Select>
+                        <div class="check">
+                            <Form.Check.Input />
+                            <Form.Check.Label>Incluir historico</Form.Check.Label>
+                        </div>
+                    </div>
+                    <Button variant="primary"  class="citytton">Consultar</Button>
+                </div>
+                
+            );
+        } else {
+            return <p className="text-center">Cargando ciudades...</p>;
+        }
+    }
+}
 
 export default CitySection
--- src/componentes/Search/Search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from "react-bootstrap/Button";
-
-const Search  = () => (
-    
-    <Button variant="primary">Consultar</Button>
-)
-
-export default Search
